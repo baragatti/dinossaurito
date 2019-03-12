ae06e9a807679edc97d648bd0252996a61febed4
Lucas <kronoxgames@gmail.com>
2019-03-11 21:27:50 -0300

    Configura o projeto para usar WebPack

--- app/index.ts
+++
@@ -1,7 +0,0 @@
-import Game from "./Game";
-
-const game = new Game();
-
-alert(`doix gudan    aaa`);
-
-// npm run check-types -- --watch
---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+// import Game from "./Game";
+
+// const game = new Game();
+
+alert(`doix gudan  xzzzz`);
+
+// npm run check-types -- --watch
